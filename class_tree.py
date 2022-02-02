a71d9b8586fa5561b5e897b1e15f66c9bdaaa5f5
Mykhailo Voitovych <m.voitovych21@gmail.com>
2022-02-02 16:46:55 +0200

    corrected mistakes

--- class_tree.py
+++ class_tree.py
@@ -5,21 +5,34 @@ class FishInfo:
 
     def __init__(self, name: str, price_in_uah_per_kilo: float, due_date: datetime,
                 origin: str, catch_date: datetime) -> None:
-        pass
+        self.name = name
+        self.price_in_uah_per_kilo = price_in_uah_per_kilo
+        self.due_date = due_date
+        self.origin = origin
+        self.catch_date = catch_date
 
 class Fish:
     
     def __init__(self, weight: float, age_in_months: float) -> None:
-        pass
+        self.weight = weight
+        self.age_in_months = age_in_months
 
 class FishBox:
+
     def __init__ (self, fish_info: FishInfo, weight: float, package_date: datetime, 
                   height: float, lenght: float, width: float, is_alive: bool) -> None:
-        pass
+        self.fish_info = fish_info
+        self.weight = weight
+        self.package_date = package_date
+        self.height = height
+        self.lenght = lenght
+        self.width = width
+        self.is_alive = is_alive
 
 class FishShop:
     def __init__(self, fish_boxes: dict[str: list[FishBox]], fresh_fish: dict[str: List[Fish]] ):
-        pass
+        self.fish_boxes = fish_boxes
+        self.fresh_fish = fresh_fish
 
     def get_fish_names_sorted_by_price(self) -> list[list]:
         pass
@@ -38,7 +51,6 @@ class FishShop:
 
     def get_fresh_fish_names_sorted_by_price(self) -> list[ tuple([str, bool, float]) ]:
         pass
-
 
 
     
